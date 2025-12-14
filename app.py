0e7c8a51fc321d061babd8c9be7bf391e2685873
christianzv14-coder <christianzv14@gmail.com>
2025-12-13 23:57:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,7 @@ def receive_message():
 
 # Local run only
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+    
