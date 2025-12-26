da19122f05d21bc55d4eaf7445a4815e4f412248
christianzv14-coder <christianzv14@gmail.com>
2025-12-26 20:51:27 -0300

    Update subir_planificacion.bat

--- subir_planificacion.bat
+++ subir_planificacion.bat
@@ -4,8 +4,8 @@ echo     CARGAR PLANIFICACION A CEREBRO
 echo ===========================================
 echo.
 cd backend
-echo Buscando archivo: plantilla_planificacion.xlsx...
-..\venv\Scripts\python.exe subir_excel.py plantilla_planificacion.xlsx
+echo Buscando archivo: plantilla_planificacion_v2.xlsx...
+..\venv\Scripts\python.exe subir_excel.py plantilla_planificacion_v2.xlsx
 echo.
 echo Proceso finalizado.
 pause
